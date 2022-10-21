719fb25b6e931f3887aa70ccefa390602f4fd1d5
JongJin <simono9745@gmail.com>
2022-10-21 18:09:11 +0900

    Button

--- app/src/main/java/com/example/sodaechang/MainActivity.java
+++ app/src/main/java/com/example/sodaechang/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.sodaechang;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.cafe):
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(req_01.this, req_02.class):
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
