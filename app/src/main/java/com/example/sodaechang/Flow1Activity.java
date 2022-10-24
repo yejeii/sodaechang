3f60c7e0cadd0119c1651bb628319f78f86bf562
yesie0108@gmail.com <yesie0108@gmail.com>
2022-10-24 16:32:47 +0900

    10.24 카테고리 선택시 intent로 업종명 전달 완료

---
+++ app/src/main/java/com/example/sodaechang/Flow1Activity.java
@@ -0,0 +1,33 @@
+package com.example.sodaechang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.sodaechang.databinding.ActivityFlow1Binding;
+
+public class Flow1Activity extends AppCompatActivity {
+
+    private ActivityFlow1Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFlow1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // view
+        TextView tv = binding.textView;
+
+        // MainActivity에서 건너온 intent 값 받아오기
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("업종");
+//        intent.getIntExtra("key값", 0);   int의 경우 value에 default 값을 적어준다
+
+        tv.setText(category);
+
+    }
+}
