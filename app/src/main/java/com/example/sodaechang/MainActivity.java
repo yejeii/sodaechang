6eeed382e072aaf99432cd8492ce60339326dda0
yesie0108@gmail.com <yesie0108@gmail.com>
2022-10-24 16:29:33 +0900

    10.24 카테고리 선택시 intent로 업종명 전달 완료

--- app/src/main/java/com/example/sodaechang/MainActivity.java
+++ app/src/main/java/com/example/sodaechang/MainActivity.java
@@ -4,21 +4,40 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.sodaechang.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());     // inflate() : xml에 있는 뷰를 객체화해준다
+        setContentView(binding.getRoot());      // 우리가 생성한 루트 뷰를 넘겨준다
 
-        Button button = findViewById(R.id.cafe):
+        // View
+        ImageButton Q12 = binding.cafe;
+        TextView Q12_text = binding.cafeText;
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // Intent
+        // Intent로 Activity 전환
+        /* 첫 번째에는 현재의 context 즉 'this', 두 번째에는 이동할 Activity.class 입력 */
+        Intent intent = new Intent(this, Flow1Activity.class);
+        // Intent로 값 전달 putExtra(Key, Value)
+        intent.putExtra("업종" , Q12_text.getText().toString());
+
+        Q12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(req_01.this, req_02.class):
+
+                // Intent로 값 전달
                 startActivity(intent);
             }
         });
