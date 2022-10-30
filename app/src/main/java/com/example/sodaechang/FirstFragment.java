a5d09b90dc0d81cdc74763d92a8e2da5f2c74d5a
yesie0108@gmail.com <yesie0108@gmail.com>
2022-10-30 17:22:16 +0900

    10.30 mysql 연동 작업 중 + firstFragment merge

--- app/src/main/java/com/example/sodaechang/FirstFragment.java
+++ app/src/main/java/com/example/sodaechang/FirstFragment.java
@@ -7,14 +7,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FirstFragment extends Fragment {
 
-    TextView tv_gugun, tv_dong, tv_ctgry;
+    TextView tv_gugun, tv_dong, tv_ctgry, tv_cost;
+    ImageView imageView01;
 
     // Fragment의 lifecycle 메소드 중에서 Fragment가 보여줄 View를 설정하는 메소드
     // MainActivity.java 에서 onCreate() 메소드 안에 setContentView()하듯이
@@ -31,6 +34,7 @@ public class FirstFragment extends Fragment {
         tv_gugun = view.findViewById(R.id.tv_gugun);
         tv_dong = view.findViewById(R.id.tv_dong);
         tv_ctgry = view.findViewById(R.id.tv_ctgry);
+        tv_cost = view.findViewById(R.id.tv_cost);
 
 
         // Flow1Activity.java에서 넘어온 정보 변수에 저장
@@ -42,6 +46,73 @@ public class FirstFragment extends Fragment {
         tv_gugun.setText(gugun);
         tv_dong.setText(dong);
         tv_ctgry.setText(catgry);
+
+        String text = tv_ctgry.getText().toString();
+        Log.d("mytag", text + "\n");
+
+        imageView01 = view.findViewById(R.id.imgView01);
+
+
+        //drawable 폴더에 있는 이미지 로드
+
+        if (text.equals("한식")) {
+            imageView01.setImageResource(R.drawable.a_q01);
+            tv_cost.setText("82614");
+
+
+
+        }
+        else if (text.equals("중식")) {
+            imageView01.setImageResource(R.drawable.a_q02);
+            tv_cost.setText("82457");
+
+        }
+        else if (text.equals("일식")) {
+            imageView01.setImageResource(R.drawable.a_q03);
+            tv_cost.setText("94032");
+
+
+        }
+        else if (text.equals("분식")) {
+            imageView01.setImageResource(R.drawable.a_q04);
+            tv_cost.setText("60213");
+
+
+        }
+        else if (text.equals("치킨/오리요리")) {
+            imageView01.setImageResource(R.drawable.a_q05);
+            tv_cost.setText("56810");
+
+
+        }
+        else if (text.equals("양식")) {
+            imageView01.setImageResource(R.drawable.a_q06);
+            tv_cost.setText("87080");
+
+
+        }
+        else if (text.equals("아이스크림/피자/패스트푸드")) {
+            imageView01.setImageResource(R.drawable.a_q07);
+            tv_cost.setText("72362");
+
+
+        }
+        else if (text.equals("제과제빵/떡/케이크")) {
+            imageView01.setImageResource(R.drawable.a_q08);
+            tv_cost.setText("118503");
+
+        }
+        else if (text.equals("주점")) {
+            imageView01.setImageResource(R.drawable.a_q09);
+            tv_cost.setText("101679");
+
+        }
+        else if (text.equals("카페/디저트")){
+            imageView01.setImageResource(R.drawable.a_q12);
+            tv_cost.setText("94193");
+
+        }
+
 
         // 생성된 View 객체를 리턴
         return view;
